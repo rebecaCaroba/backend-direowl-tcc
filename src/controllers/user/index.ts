92ebdbcae42624adf11ce6462383cb15db6cc30f
rebecaCaroba <rebecacaroba@gmail.com>
2024-06-04 11:27:46 -0300

    Rota register

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -8,7 +8,7 @@ interface UserType {
     password: string,
 }
 
-export async function login(req: Request, res: Response): Promise<Response>{
+export async function login(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
 
     try {
@@ -17,9 +17,9 @@ export async function login(req: Request, res: Response): Promise<Response>{
         const query = `SELECT * FROM users WHERE email = ? AND password = ?`
         const [result]: [ResultSetHeader & UserType[], FieldPacket[]] = await mysql.execute(query, [email, password])
 
-        if(result.length < 1) {
+        if (result.length < 1) {
             return res.status(400).json({
-                message: 'Email ou senha inválidos',  
+                message: 'Email ou senha inválidos',
             })
         }
 
@@ -28,10 +28,48 @@ export async function login(req: Request, res: Response): Promise<Response>{
             message: 'Login realizado',
         })
 
-    }catch (err) {
+    } catch (err) {
         return res.status(500).json({
             message: 'Ocorreu um erro ao tentar fazer o login :(',
             err
         })
+    }
+}
+
+export async function register(req: Request, res: Response): Promise<Response> {
+    const { username, email, password } = req.body
+
+    try {
+        const mysql = await MySQL()
+        const checkQuery = `SELECT * FROM users WHERE email = ?`
+        const [userExist]: [ResultSetHeader & UserType[], FieldPacket[]] = await mysql.execute(checkQuery, [email])
+
+        if (userExist.length > 0) {
+            await mysql.end()
+
+            return res.status(409).json({ message: 'Usuário já cadastrado', })
+        }
+
+        const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`;
+        const [result]: [ResultSetHeader, FieldPacket[]] = await mysql.execute(query, [username, email, password])
+
+        await mysql.end()
+
+        if(!result) {
+            return res.status(501).json({
+                message: "Erro ao criar o usuário.",
+            });
+        }
+
+
+        return res.status(201).json({
+            message: "O usuário foi cadastrado com sucesso.",
+            user: {
+                username
+            }
+        });
+
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro interno do servidor :(', err})
     }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,6 @@ import * as controllerUser from '../controllers/user'
 const router = Router()
 
 router.post('/login', controllerUser.login)
-
+router.post('/register', controllerUser.register)
 
 export { router }
